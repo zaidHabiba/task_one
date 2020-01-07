0119d2ea54570c56c2e2e2234247dd634b96f270
zaidHabiba <zaid-habiba@hotmail.com>
2020-01-07 16:31:05 +0200

    Fix bugs

--- app/log_middleware.py
+++ app/log_middleware.py
@@ -8,5 +8,8 @@ class LoggingMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-        logging.debug({"response_data": response.data, "status_code": response.status_code})
+        try:
+            logging.debug({"response_data": response.data, "status_code": response.status_code})
+        except Exception:
+            logging.debug({"status_code": response.status_code})
         return response
