07ba621d93a4a64d0011cd68a197571f5e8e6203
zaidHabiba <zaid-habiba@hotmail.com>
2020-01-07 15:55:05 +0200

    Edit DB

--- settings/base.py
+++ settings/base.py
@@ -55,8 +55,12 @@ WSGI_APPLICATION = 'wsgi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tast_one',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
